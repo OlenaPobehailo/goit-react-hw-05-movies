6ec1f37cba8b3516b18cd88e0fd3b3ad95f8bd80
OlenaPobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2023-11-01 19:46:46 +0000

    Deploying to gh-pages from @ OlenaPobehailo/goit-react-hw-05-movies@25ff150701df56d245ccab54d04eda0adb4a7433 🚀

---
+++ static/js/842.0c5bf762.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[842],{842:function(e,n,t){t.r(n);var c=t(87),o=t(184);n.default=function(){return(0,o.jsxs)("div",{className:"flex-column-container",children:[(0,o.jsx)("h1",{children:"Page not found"}),(0,o.jsxs)("h2",{children:["You can go to",(0,o.jsx)(c.rU,{to:"/",children:" Home page"})]})]})}}}]);
+//# sourceMappingURL=842.0c5bf762.chunk.js.map
